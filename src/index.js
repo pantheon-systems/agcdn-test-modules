22b164df448906db6ff6610667d46050a1ad6809
Tom Mount <tmountjr@gmail.com>
2020-10-15 15:16:47 -0400

    complete the rest of the test suite

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import * as https from '../lib/https'
+const https = require('../lib/https.js');
+const redirect = require('../lib/redirect.js');
+const requestHeader = require('../lib/requestHeader.js');
+const responseHeader = require('../lib/responseHeader.js');
+const robots = require('../lib/robots.txt.js');
+const status = require('../lib/status.js');
 
-export default {
-  https
-}
+module.exports = {
+  https,
+  redirect,
+  requestHeader,
+  responseHeader,
+  robots,
+  status
+};
